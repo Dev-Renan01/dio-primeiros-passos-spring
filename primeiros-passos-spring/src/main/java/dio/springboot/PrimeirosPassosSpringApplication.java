09cf147b21a1561426795863ce16aae9792b649f
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-02 13:54:24 -0300

    Projeto criado!

---
+++ primeiros-passos-spring/src/main/java/dio/springboot/PrimeirosPassosSpringApplication.java
@@ -0,0 +1,14 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PrimeirosPassosSpringApplication.class, args);
+		System.out.println("Projeto criado!");
+	}
+}
